7e18adca7142f3ee31b5dbaa75a2700a7fd949db
dabin7 <brian421@naver.com>
2022-08-12 15:48:50 +0900

    8,12

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import Button from "./Button";
+import { useState, useEffect } from "react";
+function Hello() {
+  useEffect(() => {
+    console.log("hi");
+    return () => console.log("bye :(");
+  }, []);
+  return <h1>Hello</h1>;
+}
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <h1>Welcome back</h1>
-      <Button text={"Continue"} />
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
